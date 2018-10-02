b0ef4e18845f9073093693676e0339dbc9782d9a
rohan0010 <rohancool3845@gmail.com>
2018-10-02 21:25:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rohan/basicphrassessapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.rohan.basicphrassessapp;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void buttonTapped(View view) {
+
+        int id = view.getId();
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.example.rohan.basicphrassessapp");
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
+        mplayer.start();
+
+        Log.i("button tapped", ourId);
+
+    }
+
+}
